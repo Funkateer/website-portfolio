1ac57de9c49d294abdd74f2ae667f1bbaa4f5b04
alà eddin abugrara <alaeddin.abugrara@gmail.com>
2019-03-30 19:16:08 +0100

    change size of indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,86 +1,86 @@
 (function() {
-    // VARIABLES
-    var $form = document.querySelector('#contact-form');
-    var $emailInput = document.querySelector('#contact-email');
-    var $phoneInput = document.querySelector('#contact-tel');
-    var $messageInput = document.querySelector('#contact-mes');
+  // VARIABLES
+  var $form = document.querySelector('#contact-form');
+  var $emailInput = document.querySelector('#contact-email');
+  var $phoneInput = document.querySelector('#contact-tel');
+  var $messageInput = document.querySelector('#contact-mes');
 
-    // validates input in email form
-    function validateEmail() {
-        var value = $emailInput.value;
+  // validates input in email form
+  function validateEmail() {
+    var value = $emailInput.value;
 
-        if (!value) {
-            showErrorMessage($emailInput, 'Email required');
-            return false;
-        }
-
-        if (value.indexOf('@') === -1 || value.indexOf('.') === -1) {
-            showErrorMessage($emailInput, 'Enter a valid e-mail: example@here.com');
-            return false;
-        }
-
-        showErrorMessage($emailInput, null);
-        return true;
+    if (!value) {
+      showErrorMessage($emailInput, 'Email required');
+      return false;
     }
 
-    // validates input in phone form
-    function validatePhone() {
-        var value = $phoneInput.value;
-
-        if (/\D/.test(value)) {
-            showErrorMessage($phoneInput, 'Enter a valid Phone number: 123-123-1234');
-            return false;
-        }
-
-        showErrorMessage($phoneInput, null);
-        return true;
+    if (value.indexOf('@') === -1 || value.indexOf('.') === -1) {
+      showErrorMessage($emailInput, 'Enter a valid e-mail: example@here.com');
+      return false;
     }
 
-    // validates input in message form
-    function validateMessage() {
-        var value = $messageInput.value;
+    showErrorMessage($emailInput, null);
+    return true;
+  }
 
-        if (!value) {
-            showErrorMessage($messageInput, 'Message required');
-            return false;
-        }
+  // validates input in phone form
+  function validatePhone() {
+    var value = $phoneInput.value;
 
-        showErrorMessage($messageInput, null);
-        return true;
+    if (/\D/.test(value)) {
+      showErrorMessage($phoneInput, 'Enter a valid Phone number: 123-123-1234');
+      return false;
     }
 
-    // error messages
-    function showErrorMessage($input, message) {
-        var $container = $input.parentElement;
-        var error = $container.querySelector('.error-message');
+    showErrorMessage($phoneInput, null);
+    return true;
+  }
 
-        if (error) {
-            $container.removeChild(error);
-        }
+  // validates input in message form
+  function validateMessage() {
+    var value = $messageInput.value;
 
-        if (message) {
-            error = document.createElement('div');
-            error.classList.add('error-message');
-            error.innerText = message;
-            $container.appendChild(error);
-        }
+    if (!value) {
+      showErrorMessage($messageInput, 'Message required');
+      return false;
     }
 
-    function validateForm() {
-        var isValidEmail = validateEmail();
-        var isValidPhone = validatePhone();
-        var isValidMessage = validateMessage();
-        return isValidEmail && isValidPhone && isValidMessage;
+    showErrorMessage($messageInput, null);
+    return true;
+  }
+
+  // error messages
+  function showErrorMessage($input, message) {
+    var $container = $input.parentElement;
+    var error = $container.querySelector('.error-message');
+
+    if (error) {
+      $container.removeChild(error);
     }
 
-    $form.addEventListener('submit', (e) => {
-        e.preventDefault(); // Do not submit to the server
-        if (validateForm()) {
-            alert('Success!');
-        }
-    });
+    if (message) {
+      error = document.createElement('div');
+      error.classList.add('error-message');
+      error.innerText = message;
+      $container.appendChild(error);
+    }
+  }
 
-    $emailInput.addEventListener('input', validateEmail);
-    $phoneInput.addEventListener('input', validatePhone);
-    $messageInput.addEventListener('input', validateMessage);
+  function validateForm() {
+    var isValidEmail = validateEmail();
+    var isValidPhone = validatePhone();
+    var isValidMessage = validateMessage();
+    return isValidEmail && isValidPhone && isValidMessage;
+  }
+
+  $form.addEventListener('submit', (e) => {
+    e.preventDefault(); // Do not submit to the server
+    if (validateForm()) {
+      alert('Success!');
+    }
+  });
+
+  $emailInput.addEventListener('input', validateEmail);
+  $phoneInput.addEventListener('input', validatePhone);
+  $messageInput.addEventListener('input', validateMessage);
 })();
